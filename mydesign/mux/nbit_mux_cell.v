76c7a6690470d546ea2501d7425573c0ce21d93c
BinaryBrainer <hongoccuong002@gmail.com>
2026-07-05 21:03:15 +0700

    verified nbit_mux_cell

--- mydesign/mux/nbit_mux_cell.v
+++ mydesign/mux/nbit_mux_cell.v
@@ -1,33 +1,36 @@
 //stautus: complete, unverified
 
-`include "mydesign/mux/mux_cell"
+`include "mydesign/mux/mux_cell.v"
 
 module nbit_mux_cell #(
     parameter LENGTH = 32,
     parameter WIDTH = 32
 ) (
-    input [WIDTH-1:0] src[LENGTH-1:0],
+    input [WIDTH-1:0] src [LENGTH-1:0],
     input [$clog2(LENGTH)-1:0] sel,
 
     output [WIDTH-1:0] data
 );
 
-/*
-+---------+---------+
-| Sel[1:0]| Output  |
-+---------+---------+
-|   00    | src0    |
-|   01    | src1    |
-|   10    | src2    |
-|   11    | src3    |
-+---------+---------+
-*/
 
 
 generate
     genvar i;
-    for (i = 0; i < WIDTH; i = i + 1) begin
-        
+    for (i=0; i<WIDTH; i=i+1) begin : GEN_MUX
+
+        wire  column [LENGTH-1:0]; 
+        genvar j;
+        for (j=0; j<LENGTH; j=j+1) begin : GEN_COL
+            assign column[j] = src[j][i];
+        end
+
+        mux_cell #(
+            .LENGTH(LENGTH)
+        ) u_mux (
+            .src(column),
+            .sel(sel),
+            .data(data[i])
+        );
     end
 endgenerate
 
